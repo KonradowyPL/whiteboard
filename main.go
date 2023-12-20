ddd36e972bb8ba1c23d93941b4040ec74bb79ab9
KonradowyPL <konrad.chrzaszczak@gmail.com>
2023-12-20 18:24:11 +0100

    refactor, zoomier zoom move grid to game.world add world.Update function called at 20Hz change game.Update to be called at 60Hz

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import (
 	"image/color"
 	"log"
 	"math/rand"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"mrogalski.eu/go/vec"
 )
 
+// TODO
+// low: clean up variable declatarions
 type Game struct {
 	camera    Camera
 	lastMouse vec.Vec
+
+	world World
+}
+
+type World struct {
+	lastUpdate int64
+
+	grid [][]object
 }
 
 type object struct {
@@ -25,13 +36,27 @@ type Camera struct {
 	zoom float64
 }
 
-var grid [][]object
+var zoom = struct {
+	min float64
+	max float64
+}{0.05, 4}
 
-func (g *Game) Update() error {
+// converts screen position in pixels to world space
+func (g *Game) screenToWorldspace(pos vec.Vec) vec.Vec {
+	return pos.Scale(1 / g.camera.zoom).Add(g.camera.pos)
+}
 
-	x, y := ebiten.CursorPosition()
-	cursorPos := vec.New(float64(x), float64(y))
+// converts world cordinates to screen position in pixels
+func (g *Game) worldToScreen(pos vec.Vec) vec.Vec {
+	return g.worldToGlobal(pos).Scale(g.camera.zoom)
+}
 
+// makes cordinates relateive to camerea
+func (g *Game) worldToGlobal(pos vec.Vec) vec.Vec {
+	return pos.Sub(g.camera.pos)
+}
+
+func (g *Game) moveCamera(cursorPos vec.Vec) {
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButton0) {
 		delta := cursorPos.Sub(g.lastMouse)
 
@@ -41,41 +66,75 @@ func (g *Game) Update() error {
 		if dy != 0 {
 
 			// check posiiton mouse is hovering at before change
-			before := cursorPos.Scale(1 / g.camera.zoom).Add(g.camera.pos)
+			before := g.screenToWorldspace(cursorPos)
 
-			g.camera.zoom += dy
-			if g.camera.zoom <= 0 {
-				g.camera.zoom = 1
+			g.camera.zoom *= dy*.1 + 1
+			// check if within bounds
+			if g.camera.zoom <= zoom.min {
+				g.camera.zoom = zoom.min
+			} else if g.camera.zoom >= zoom.max {
+				g.camera.zoom = zoom.max
 			}
 
 			// check posiiton mouse is hovering at after change
-			after := cursorPos.Scale(1 / g.camera.zoom).Add(g.camera.pos)
+			after := g.screenToWorldspace(cursorPos)
 
 			// calculate diffrence and add it to camera pos
 			// this is to coursor stays in the same place after zooming
 			diff := before.Sub(after)
 			g.camera.pos = g.camera.pos.Add(diff)
+
+			// TODO
+			// low: fixing camera alignment proces proably can be optymalised
 		}
 
 		g.camera.pos = g.camera.pos.Add(delta.Scale(-1 / g.camera.zoom))
 	}
+}
+
+func (w *World) Update() error {
+	return nil
+}
+
+func (g *Game) Update() error {
+	now := time.Now().UnixNano()
+
+	// delta time in seconds
+	// 1e-9 = nano prefix
+	deltaTime := float64((now - g.world.lastUpdate)) * 1e-9
+
+	// 20Hz
+	if deltaTime >= 0.05 {
+		g.world.lastUpdate = now
+		// update world
+		// world is ran on 20Hz while
+		// while game is rendered at
+		err := g.world.Update()
+		if err != nil {
+			return err
+		}
+	}
+
+	x, y := ebiten.CursorPosition()
+	cursorPos := vec.New(float64(x), float64(y))
+	g.moveCamera(cursorPos)
 
 	g.lastMouse = cursorPos
-
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	for x, row := range grid {
+	for x, row := range g.world.grid {
 		for y, tile := range row {
 			square := ebiten.NewImage(32, 32)
 			square.Fill(tile.color)
 
 			opts := &ebiten.DrawImageOptions{}
 
-			opts.GeoM.Translate(float64(x)*32-g.camera.pos.X, float64(y)*32-g.camera.pos.Y)
+			screenPos := g.worldToGlobal(vec.New(float64(x)*32, float64(y)*32))
 
+			opts.GeoM.Translate(screenPos.X, screenPos.Y)
 			opts.GeoM.Scale(g.camera.zoom, g.camera.zoom)
 
 			screen.DrawImage(square, opts)
@@ -84,7 +143,6 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	}
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("%dTPS\n%.2fFPS", ebiten.TPS(), ebiten.ActualFPS()))
-
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -92,22 +150,24 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	game := &Game{}
+	game.camera.zoom = 1
 
 	for i := 0; i < 10; i++ {
 		row := make([]object, 10)
 		for j := 0; j < 10; j++ {
 			r, g, b := uint8(rand.Intn(0xff)), uint8(rand.Intn(0xff)), uint8(rand.Intn(0xff))
-			row[j] = object{color.RGBA{r, g, b, 0}}
+			row[j] = object{color.RGBA{r, g, b, 0xff}}
 		}
-		grid = append(grid, row)
+		game.world.grid = append(game.world.grid, row)
 	}
 
-	ebiten.SetTPS(20)
+	ebiten.SetTPS(60)
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.SetWindowTitle("Hello, World!")
 
-	if err := ebiten.RunGame(&Game{Camera{vec.New(0, 0), 1}, vec.New(0, 0)}); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
